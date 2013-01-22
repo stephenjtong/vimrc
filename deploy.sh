a8589ea94888e02c42a99b5d77aca1cbb360c252
Stephen <stephen.j.tong@gmail.com>
2013-01-22 21:43:52 +0800

    我就是提交一次看看我还能不能连上来。日子没法过了，逼人肉身翻墙吗？

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-
 rm ~/.vimrc
 ln -s $PWD/.vimrc ~/.vimrc
-#rm -rf ~/.vim/bundle/vundle
-#git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-#vim +BundleInstall +qa
+rm -rf ~/.vim/bundle/vundle
+git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+vim +BundleInstall +qa
