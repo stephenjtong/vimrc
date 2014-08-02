62235f65d55668800e04bc35702ebe155693d14b
Stephen <stephen.j.tong@gmail.com>
2014-08-02 13:48:01 +0800

    更新github vundle地址为 https

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 rm ~/.vimrc
 ln -s $PWD/.vimrc ~/.vimrc
 rm -rf ~/.vim/bundle/vundle
-git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qa
